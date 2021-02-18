2af45611270a8cc81ec58c41959caedeb78c8765
Nicholas Tan <nicholas.tan@cognizant.com>
2021-02-19 03:36:52 +0800

    generate password

---
+++ include/funcs/funcs.php
@@ -0,0 +1,26 @@
+<?php
+
+	function generatePassword(){
+			$password_list = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-_+<>[]{}';
+		    $password_list_length = strlen($password_list) - 1; //strlen starts from 0 so to get number of characters deduct 1
+		     
+		    $lower_case = 'abcdefghijklmnopqrstuvwxyz';
+		    $upper_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		    $numbers = '1234567890';
+		    $special_symbols = '!?~@#-_+<>[]{}';
+
+		    $password = $lower_case[rand(0, (strlen($lower_case) - 1))];
+		    $password .= $upper_case[rand(0, (strlen($upper_case) - 1))];
+		    $password .= $numbers[rand(0, (strlen($numbers) - 1))];
+		    $password .= $special_symbols[rand(0, (strlen($special_symbols) - 1))];
+
+		    for ($i = 0; $i < 4; $i++) {
+		        $n = rand(0, $password_list_length); // get a random character from the string with all characters
+		        $password .= $password_list[$n]; // add the character to the password string
+		    }
+    		$pass = str_shuffle($password);
+     
+    		return $pass; // return the generated password
+		}
+	
+?>
