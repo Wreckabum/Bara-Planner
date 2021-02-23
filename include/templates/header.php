76e858fa530f41bed2b0e39416eb4c183b9e417d
wenhang <71421591+wenhang361@users.noreply.github.com>
2021-02-23 15:54:46 +0800

    add style to header

--- include/templates/header.php
+++ include/templates/header.php
@@ -3,7 +3,84 @@
 		$account = get_account($_SESSION["id"]);
 	}
 ?>
-<div style='float:left;'>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<style>
+.navbar .dropdown-toggle, .navbar .dropdown-menu a {
+    cursor: pointer;
+}
+
+.navbar .dropdown-item.active, .navbar .dropdown-item:active {
+    color: inherit;
+    text-decoration: none;
+    background-color: inherit;
+}
+
+.navbar .dropdown-item:focus, .navbar .dropdown-item:hover {
+    color: #16181b;
+    text-decoration: none;
+    background-color: #f8f9fa;
+}
+
+@media (min-width: 767px) {
+    .navbar .dropdown-toggle:not(.nav-link)::after {
+        display: inline-block;
+        width: 0;
+        height: 0;
+        margin-left: .5em;
+        vertical-align: 0;
+        border-bottom: .3em solid transparent;
+        border-top: .3em solid transparent;
+        border-left: .3em solid;
+    }
+}
+</style>
+
+<div class="navbar navbar-expand-md navbar-dark bg-dark mb-4" role="navigation">
+    <a class="navbar-brand" href="#">Welcome </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+			    
+            		<a class="nav-link"  href='view_account.php?a=<?= $account->id ?>'>
+						<?= $account->get_name() ?>
+					</a>
+		        </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="view_all.php" target="_blank">View All</a>
+            </li>
+			<?php
+				if($account->is_admin()){
+			?>
+            <li class="nav-item">
+                <a class="nav-link" href="add_student.php">Add a new student</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" href="add_faculty.php">Add a new faculty member</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" href="add_major.php">Add a new major</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" href="add_project.php">Add a new project</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" href="import.php">Import</a>
+            </li>
+
+			<?php
+				}
+			?>
+        </ul>
+		<a class="nav-link" href="logout.php">Logout</a>
+    </div>
+</div>
+<!-- <div style='float:left;'>
 	Welcome 
 	<a href='#'>
 		<a href='view_account.php?a=<?= $account->id ?>'>
@@ -28,6 +105,9 @@
 			<a href='add_project.php'>
 				[ Add a new project ]
 			</a>
+			<a href='import.php'>
+				[ Import file ]
+			</a>
 	<?php
 		}
 	?>
@@ -36,6 +116,6 @@
 	<a href='logout.php'>
 		Logout
 	</a>
-</div>
+</div> -->
 <br />
 <br />
