85a75854c159dfc3bd5bff5b87091ff0c093ebc2
wenhang <71421591+wenhang361@users.noreply.github.com>
2021-03-02 20:11:29 +0800

    Add files via upload

--- home.php
+++ home.php
@@ -33,277 +33,286 @@
 			$ft = get_account(10280958);
 			$pt = get_account(10213701);
 		?>
-		<table class='profile_details' style='width:30%; border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Name:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $super->get_name() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Phone Number:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $super->get_phone() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					SIM E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $super->get_sim_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Personal E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $super->get_personal_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Account Type:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $super->get_account_type() ?>
-				</td>
-			</tr>
-			<tr>
-		</table>
-		<br />
-		<table class='profile_details' style='width:30%; border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Name:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $admin->get_name() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Phone Number:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $admin->get_phone() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					SIM E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $admin->get_sim_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Personal E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $admin->get_personal_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Account Type:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $admin->get_account_type() ?>
-				</td>
-			</tr>
-		</table>
-		<br />
-		<table class='profile_details' style='width:30%; border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Name:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $prof->get_name() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Phone Number:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $prof->get_phone() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					SIM E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $prof->get_sim_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Personal E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $prof->get_personal_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Account Type:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $prof->get_account_type() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Major:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $prof->get_majors(true) ?>
-				</td>
-			</tr>
-		</table>
-		<br />
-		<table class='profile_details' style='width:30%; border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Name:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_name() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Phone Number:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_phone() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					SIM E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_sim_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Personal E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_personal_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Account Type:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_account_type() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Major:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_majors() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Year:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_year() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Quarter:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_quarter() ?>
-				</td>
-			</tr>
-		</table>
-		<br />
-		<table class='profile_details' style='width:30%; border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Name:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $pt->get_name() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Phone Number:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $pt->get_phone() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					SIM E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $pt->get_sim_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Personal E-Mail:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $pt->get_personal_email() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Account Type:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $pt->get_account_type() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Major:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $pt->get_majors() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Year:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_year() ?>
-				</td>
-			</tr>
-			<tr>
-				<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
-					Quarter:
-				</td>
-				<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
-					<?= $ft->get_quarter() ?>
-				</td>
-			</tr>
-		</table>
+		<div class='container'>
+			<div class='col-md-6' style='float:left'>
+				<table class='profile_details' style=' border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Name:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $super->get_name() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Phone Number:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $super->get_phone() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							SIM E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $super->get_sim_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Personal E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $super->get_personal_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Account Type:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $super->get_account_type() ?>
+						</td>
+					</tr>
+					<tr>
+				</table>
+				</div>
+			<div class='col-md-6' style='float:left'>
+				<table class='profile_details' style=' border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Name:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $admin->get_name() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Phone Number:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $admin->get_phone() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							SIM E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $admin->get_sim_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Personal E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $admin->get_personal_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Account Type:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $admin->get_account_type() ?>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div  class='col-md-6' style='float:left'>
+				<table class='profile_details' style=' border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Name:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $prof->get_name() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Phone Number:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $prof->get_phone() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							SIM E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $prof->get_sim_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Personal E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $prof->get_personal_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Account Type:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $prof->get_account_type() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Major:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $prof->get_majors(true) ?>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div  class='col-md-6' style='float:left'>
+				<table class='profile_details' style=' border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Name:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_name() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Phone Number:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_phone() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							SIM E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_sim_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Personal E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_personal_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Account Type:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_account_type() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Major:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_majors() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Year:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_year() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Quarter:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_quarter() ?>
+						</td>
+					</tr>
+				</table>
+            </div>
+
+			<div  class='col-md-6' style='float:left'>
+				<table class='profile_details' style=' border-collapse:collapse; border:1px #000 solid; padding-left:7px;'>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Name:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $pt->get_name() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Phone Number:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $pt->get_phone() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							SIM E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $pt->get_sim_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Personal E-Mail:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $pt->get_personal_email() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Account Type:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $pt->get_account_type() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Major:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $pt->get_majors() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Year:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_year() ?>
+						</td>
+					</tr>
+					<tr>
+						<td style='width:30%; border:1px #000 solid; padding-left:7px;'>
+							Quarter:
+						</td>
+						<td style='width:70%; border:1px #000 solid; padding-left:7px;'>
+							<?= $ft->get_quarter() ?>
+						</td>
+					</tr>
+				</table>
+            </div>
+		</div>
 	</body>
 </html>
 <?php
